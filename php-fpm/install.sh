97b910573384dc13b51e147b631229bf1518394b
nizar <nizarucsc@gmail.com>
2019-03-21 17:42:51 +0530

    add php-fpm install

--- php-fpm/install.sh
+++ php-fpm/install.sh
@@ -1,11 +1,10 @@
 echo 'install php dependencies'
 
 apt-get update && \
-apt-get install  php7.2-gd  php7.2-mysql php7.2-intl php7.2-mbstring php7.2-sqlite3;
+apt-get install php-fpm  php7.2-gd  php7.2-mysql php7.2-intl php7.2-mbstring php7.2-sqlite3;
 
 mkdir /etc/php/7.2/fpm/conf.d
-cp ./php-ini-overrides.ini /etc/php/7.2/fpm/conf.d/99-overrides.ini
-cp ./overrides.conf /etc/php/7.1/fpm/pool.d/z-overrides.conf
+
 
 git clone https://github.com/moe-lk/sis.git
 
@@ -13,13 +12,15 @@ mkdir /src
 mkdir /var/www/html 
 mkdir /etc/php/7.2/conf.d
 
-cp sis/php-fpm/app /src
+cat ./php-ini-overrides.ini >> /etc/php/7.2/fpm/conf.d/99-overrides.ini
+cat ./overrides.conf >> /etc/php/7.2/fpm/pool.d/z-overrides.conf
 
- rm -rf /var/www/html && mv /src /var/www/html &&\
-    find /var/www/html/ -type d -exec chmod 777 {} \; &&\
-    find /var/www/html/ -type f -exec chmod 644 {} \; &&\
-    find /var/www/html/tmp -type d -exec chmod 777 {} \; &&\
-    find /var/www/html/logs -type d -exec chmod 777 {} \; 
+cp sis/php-fpm/app/* /var/www/html
+
+find /var/www/html/ -type d -exec chmod 777 {} \; &&\
+find /var/www/html/ -type f -exec chmod 644 {} \; &&\
+find /var/www/html/tmp -type d -exec chmod 777 {} \; &&\
+find /var/www/html/logs -type d -exec chmod 777 {} \; 
 
 
 chown -R www-data:www-data /var/www
